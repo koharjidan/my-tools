b40c7a21e63be8436db6950cb2039187854d3def
Kohar Jidan <kohar.jidan@gmail.com>
2025-08-03 11:26:59 +0700

    install to $HOME/.local/bin as on tumbleweed

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo "Install tools to $HOME/bin"
+echo "Install tools to $HOME/.local/bin"
 
 chmod +x git/sync.sh
 chmod +x git/run-sync.sh
 
-ln -sf `pwd`/git/sync.sh $HOME/bin/sync-git
-ln -sf `pwd`/git/run-sync.sh $HOME/bin/run-sync
+ln -sf `pwd`/git/sync.sh $HOME/.local/bin/sync-git
+ln -sf `pwd`/git/run-sync.sh $HOME/.local/bin/run-sync

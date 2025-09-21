72b685ce17b04ce9dd567b80964d4f0b9d16c4e0
Kohar Jidan <kohar.jidan@gmail.com>
2025-09-22 04:56:57 +0700

    php add phpbrew; install fix

--- install.sh
+++ install.sh
@@ -6,8 +6,10 @@ chmod +x git/sync.sh
 chmod +x git/run-sync.sh
 chmod +x git/github-sync.sh
 chmod +x php/composer.phar
+chmod +x php/phpbrew.phar
 
 ln -sf `pwd`/git/sync.sh $INSDIR/sync-git
 ln -sf `pwd`/git/run-sync.sh $INSDIR/run-sync
 ln -sf `pwd`/git/github-sync.sh $INSDIR/github-sync
 ln -sf `pwd`/php/composer.phar $INSDIR/composer
+ln -sf `pwd`/php/phpbrew.phar $INSDIR/phpbrew
